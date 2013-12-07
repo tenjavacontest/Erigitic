a6587115ced48f08a9cee6c08624fc64c261e137
Erigitic <emgrandt@gmail.com>
2013-12-07 12:29:23 -0600

    Tweaked commands and fixed file saving

--- Erigitic/src/com/erigitic/main/MobTalk.java
+++ Erigitic/src/com/erigitic/main/MobTalk.java
@@ -1,14 +1,10 @@
 package com.erigitic.main;
 
 import com.erigitic.listener.MTListener;
-import net.minecraft.server.v1_7_R1.EntityLiving;
-import net.minecraft.server.v1_7_R1.EntityVillager;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.plugin.PluginManager;
@@ -16,19 +12,39 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MobTalk extends JavaPlugin {
 
     private final String VERSION = "Version 1.0.0";
+    private final String mainDir = "plugins/MobTalk/";
+
+    private File mobLoc = new File(mainDir + "MobLoc.loc");
 
     private MTListener mtListener;
+    private FileReader reader;
 
     public void onEnable() {
 
         PluginManager pm = getServer().getPluginManager();
 
-        getLogger().info("MobTalk " + VERSION + " Succesfully Enabled.");
+        getLogger().info("MobTalk " + VERSION + " Successfully Enabled.");
 
-        mtListener = new MTListener(this);
+        //Creates a file for storing mob locations
+        new File(mainDir).mkdir();
+
+        if (!mobLoc.exists()) {
+
+            try {
+                mobLoc.createNewFile();
+            } catch (IOException ex) {
+
+            }
+        }
+
+        reader = new FileReader(this);
+        mtListener = new MTListener(this, reader);
 
         pm.registerEvents(mtListener, this);
 
@@ -36,7 +52,7 @@ public class MobTalk extends JavaPlugin {
 
     public void onDisable() {
 
-        getLogger().info("MobTalk " + VERSION + " Succesfully Disabled.");
+        getLogger().info("MobTalk " + VERSION + " Successfully Disabled.");
 
     }
 
@@ -44,18 +60,36 @@ public class MobTalk extends JavaPlugin {
 
         Player p = (Player) sender;
 
-        if (cmdLabel.equalsIgnoreCase("test")) {
+        if (cmdLabel.equalsIgnoreCase("create")) {//Spawns mob. Will allow for sender to choose mob of choice
 
-            Location loc = p.getLocation();
-            World world = loc.getWorld();
-            Villager v = world.spawn(loc, Villager.class);
+            if (args.length == 1) {
+                Location loc = p.getLocation();
+                World world = loc.getWorld();
+                Villager v = world.spawn(loc, Villager.class);
 
-            v.addPotionEffect(new PotionEffect((PotionEffectType.JUMP), 999999, 128));
-            v.addPotionEffect(new PotionEffect((PotionEffectType.SLOW), 999999, 6));
+                v.setCustomName(args[0]);
+                reader.writeMobMessage(args[0], "", mobLoc);
+
+                v.addPotionEffect(new PotionEffect((PotionEffectType.JUMP), 999999, 128));
+                v.addPotionEffect(new PotionEffect((PotionEffectType.SLOW), 999999, 6));
+            }
+
+        } else if (cmdLabel.equalsIgnoreCase("message")) {
+
+            if (args[0] != null) {
+
+                reader.writeMobMessage(mtListener.getMobName(), args[0], mobLoc);
+
+            }
 
         }
 
-        return false;
+        return true;
+    }
+
+    public File getMobLoc() {
+
+        return mobLoc;
     }
 
 }
