ec3bd7547093db925833096063e0dcb1dbc39a7e
Erigitic <emgrandt@gmail.com>
2013-12-07 09:48:44 -0600

    Initial Commit

---
+++ Erigitic/src/com/erigitic/Main.java
@@ -0,0 +1,17 @@
+package com.erigitic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public void onEnable() {
+
+
+    }
+
+    public void onDisable() {
+
+
+    }
+
+}
